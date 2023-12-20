1d857524f63eaf30adbf2691e2a91c48a5890dd2
mucahittasan <mucahittasan0@gmail.com>
2023-12-20 21:27:01 +0300

    Onboarding page design and functions created. Backend is not added yet

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,11 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
+
 export default authMiddleware({
-    publicRoutes: ['/', '/api/webhook/clerk'],
-    ignoredRoutes: ["/api/webhook/clerk"]
+  publicRoutes: ["/api/webhook/clerk"],
+
+  ignoredRoutes: ["/api/webhook/clerk"],
 });
- 
+
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
- 
--- next.config.js
+++ next.config.js
@@ -1,4 +1,32 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        serverActions: true,
+        serverComponentsExternalPackages: ["mongoose"],
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "img.clerk.com",
+            },
+            {
+                protocol: "https",
+                hostname: "images.clerk.dev",
+            },
+            {
+                protocol: "https",
+                hostname: "uploadthing.com",
+            },
+            {
+                protocol: "https",
+                hostname: "placehold.co",
+            },
+        ],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
