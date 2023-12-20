caa2f724cab4e8023e943d79cbf1cf4625caa0f5
mucahittasan <mucahittasan0@gmail.com>
2023-12-20 16:03:19 +0300

    Sign-in, Sign-out pages and authorization created

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import '../globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +16,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Topbar />
+
+        <main>
+          <LeftSidebar />
+
+          <section className='main-container'>
+            <div className='w-full max-w-4xl'>
+              {children}
+            </div>
+          </section>
+
+          <RightSidebar />
+        </main>
+
+        <Bottombar />
+      </body>
     </html>
   )
 }
---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+    <>
+      <h1 className="head-text text-left">Home</h1>
+    </>
+  )
+}
+
+export default Home
