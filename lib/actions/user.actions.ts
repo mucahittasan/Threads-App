bdb296faf65e147227ab70bf8cdfd78754c31a10
mucahittasan <mucahittasan0@gmail.com>
2023-12-21 10:10:48 +0300

    Created first action model as updateUser ✨

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,7 +1,38 @@
 "use server"
 
+import { revalidatePath } from "next/cache";
+import User from "../models/user.model";
 import { connectToDB } from "../mongoose"
 
-export async function updateUser(): Promise<void> {
+export async function updateUser(
+    userId: string,
+    username: string,
+    name: string,
+    bio: string,
+    image: string,
+    path: string
+    ): Promise<void> {
+
     connectToDB();
+
+   try {
+    await User.findOneAndUpdate(
+        {id: userId},
+        {
+            username: username.toLowerCase(),
+            name,
+            bio,
+            image,
+            onboarded: true
+        },
+        // upsert true'nun anlami; Eger id'si userId ile ayni olan bir belge yoksa o zaman userId'ye sahip belgeyi guncellemek yerine bu verilen bilgilere gore yeni bir kullanici yani belge olusturulur.
+        {upsert: true}  
+    );
+
+    if(path === "/profile/edit") {
+        revalidatePath(path);
+    }
+   } catch (error: any) {
+        throw new Error(`Failed to create/update user: ${error.message}`)
+   }
 }
