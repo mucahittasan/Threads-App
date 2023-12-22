993046464e792a625e92ef163d89ec96fb4a1ad6
mucahittasan <mucahittasan0@gmail.com>
2023-12-22 10:58:05 +0300

    Crated ThreadCard and designed but not finished

--- next.config.js
+++ next.config.js
@@ -25,6 +25,10 @@ const nextConfig = {
                 protocol: "https",
                 hostname: "placehold.co",
             },
+            {
+                protocol: "https",
+                hostname: "utfs.io",
+            }
         ],
     },
 };
