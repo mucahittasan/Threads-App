91244e12b440bac9e19cd62650f8fa72f4500f21
mucahittasan <mucahittasan0@gmail.com>
2024-01-20 11:14:25 +0300

    created: Activity page is created and done

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,9 +1,48 @@
+import { fetchUser, getActivity } from '@/lib/actions/user.actions';
+import { currentUser } from '@clerk/nextjs'
+import Image from 'next/image';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
 import React from 'react'
 
 const Page = async () => {
+
+    const user = await currentUser();
+    if (!user) return null;
+
+    const userInfo = await fetchUser(user.id);
+    if (!userInfo?.onboarded) redirect("/onboarding");
+
+    const activity = await getActivity(userInfo._id);
+
     return (
         <section>
-            <h1 className='head-text mb-10'>ACtivity</h1>
+            <h1 className='head-text mb-10'>Activity</h1>
+
+            <section className='mt-10 flex flex-col gap-5'>
+                {activity.length > 0 ? (
+                    <>
+                        {activity.map((item, i) => (
+                            <Link key={item._id} href={`/thread/${item.parentId}`}>
+                                <article className='activity-card'>
+                                    <Image
+                                        src={item.author.image}
+                                        alt='Profile picture'
+                                        width={20}
+                                        height={20}
+                                        className='rounded-full object-cover'
+                                    />
+                                    <p className='!text-small-regular text-light-1'>
+                                        <span className='mr-1 text-primary-500'>{item.author.name} </span>
+                                        replied to your thread
+                                    </p>
+                                </article>
+                            </Link>
+                        ))}
+                    </>
+                ) : <p className='!text-base-regular text-light-3'>No activity yet</p>}
+            </section>
+
         </section>
     )
 }
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose"
 import Thread from "../models/thread.model";
+import { FilterQuery, SortOrder } from "mongoose";
 
 interface UpdateUserProps {
     userId: string,
@@ -84,5 +85,84 @@ export async function fetchUserPosts(userId: string) {
         return threads;
     } catch (error:any) {
         throw new Error(`Failed to fetch user posts: ${error.message}`)
+    }
+}
+
+export async function fetchUsers({
+    userId,
+    searchString ="",
+    pageNumber = 1,
+    pageSize = 20,
+    sortBy = "desc"
+} : {
+    userId: string;
+    searchString?:string;
+    pageNumber?:number;
+    pageSize?:number;
+    sortBy?: SortOrder
+}) {
+    try {
+        
+        connectToDB();
+
+        const skipAmount = (pageNumber - 1) * pageSize;
+
+        const regex = new RegExp(searchString, "i");
+
+        const query:FilterQuery<typeof User> = {
+            id: {$ne: userId}
+        }
+
+        if(searchString.trim() !== "") {
+            query.$or = [
+                {username: {$regex: regex}},
+                {name: {$regex: regex}}
+            ]
+        }
+
+        const sortOptions = {createdAt:sortBy};
+
+        const usersQuery = User.find(query)
+            .sort(sortOptions)
+            .skip(skipAmount)
+            .limit(pageSize)
+
+        const totalUsersCount = await User.countDocuments(query);
+
+        const users = await usersQuery.exec();
+
+        const isNext = totalUsersCount > skipAmount + users.length
+
+        return {users, isNext}
+
+    } catch (error: any) {
+        throw new Error(`Failed to fetch users: ${error.messaeg}`)
+    }
+}
+
+export async function getActivity(userId: string) {
+    try {
+        connectToDB();
+
+        // find all threads created by the user
+        const userThreads = await Thread.find({author: userId});
+
+        const childThreadsId = userThreads.reduce((acc, userThread) => {
+            return acc.concat(userThread.children);
+        }, []);
+
+        const replies = await Thread.find({
+            _id: {$in: childThreadsId},
+            author: {$ne: userId}
+        }).populate({
+            path: "author",
+            model: User,
+            select: "name image _id"
+        })
+
+        return replies;
+
+    } catch (error:any) {
+        throw new Error(`Failed to fetch activity: ${error.messaeg}`)
     }
 }
